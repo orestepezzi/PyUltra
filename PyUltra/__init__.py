b4fce5e88b69b82c30e50ca53aa9dbed4be08ef3
Oreste Pezzi <oreste.pezzi@istp.cnr.it>
2025-06-11 16:00:07 +0200

    Update __init__.py

--- PyUltra/__init__.py
+++ PyUltra/__init__.py
@@ -1,12 +1,8 @@
 import PyUltra.FFTderiv
+import PyUltra.plottingrout
+import PyUltra.sims_reading
 import PyUltra.sims_spectra
 import PyUltra.sims_increments
 import PyUltra.sims_energyconv
 import PyUltra.sims_nonMaxw
 
-
-### April 10 2025
-### Package to study energy conversion and dissipation in space plasmas
-### ULTRA Project
-
-### Authors: O. Pezzi, A. Larosa, ..
