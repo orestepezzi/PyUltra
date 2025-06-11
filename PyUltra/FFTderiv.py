edf626a972b09bf830cf9168e5e0d91d05d4ae18
Oreste Pezzi <oreste.pezzi@istp.cnr.it>
2025-06-11 15:57:21 +0200

    Update FFTderiv.py

--- PyUltra/FFTderiv.py
+++ PyUltra/FFTderiv.py
@@ -2,7 +2,6 @@
 from math import *
 import numpy as np
 import matplotlib.pyplot as plt
-#import re
 
 ###########################################################################################
 #############
